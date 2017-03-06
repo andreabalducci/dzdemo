8a371ab029eee2d3fdd0e0313f1fadc7ae829cdf
Andrea Balducci <mtb.snowboard@gmail.com>
2017-03-06 19:16:55 +0100

    added webworker (to be continued)

--- src/app/dropzone.directive.ts
+++ src/app/dropzone.directive.ts
@@ -4,54 +4,25 @@ import { Directive, HostListener, ElementRef, NgZone, OnInit } from '@angular/co
   selector: '[appDropzone]'
 })
 export class DropzoneDirective implements OnInit {
-  folders = 0;
-  files = 0;
-
   constructor(private el: ElementRef, private ngZone: NgZone) {
 
   }
 
-  traverse(entry, path?: string): void {
-    path = path || '';
-    if (entry.isFile) {
-      // Get file
-      entry.file((file) => {
-        this.files++;
-        console.log('File:', path + file.name);
-      });
-    } else if (entry.isDirectory) {
-      // Get folder contents
-      console.log('Directory:', path + entry.name);
-      this.folders++;
-
-      const dirReader = entry.createReader();
-      dirReader.readEntries((entries) => {
-        for (let i = 0; i < entries.length; i++) {
-          this.traverse(entries[i], path + entry.name + '/');
-        }
-      });
-    }
-  }
-
   ngOnInit(): void {
     this.ngZone.runOutsideAngular(() => {
-      this.el.nativeElement.addEventListener('drop', (e) => {
+      this.el.nativeElement.addEventListener('drop', (e: DragEvent) => {
         e.stopPropagation();
         e.preventDefault();
 
-        this.files = 0;
-        this.folders = 0;
-
         const items = e.dataTransfer.items;
-        for (let i = 0; i < items.length; i++) {
-          const entry = items[i].webkitGetAsEntry();
-          if (entry) {
-            this.traverse(entry);
-          }
-        }
 
-        // @@TODO wait callbacks
-        console.log('Folders ', this.folders, ' Files ', this.files);
+        const worker = new Worker('scanworker.js');
+        worker.onmessage = (msg) => {
+          console.log('message from worker', msg);
+        };
+
+        worker.postMessage('start', [items]);
+
       }, false);
 
       this.el.nativeElement.ondragover = (e) => {
---
+++ src/app/scanworker.ts
@@ -0,0 +1,50 @@
+class ScanWorker {
+    folders = 0;
+    files = 0;
+
+    onmessage(e) {
+        console.log('worker start', e);
+    };
+
+    start(items: DataTransferItemList) {
+        this.files = 0;
+        this.folders = 0;
+
+        for (let i = 0; i < items.length; i++) {
+          const entry = items[i].webkitGetAsEntry();
+          if (entry) {
+            this.traverse(entry);
+          }
+        }
+
+        // @@TODO wait callbacks
+        console.log('Folders ', this.folders, ' Files ', this.files);
+    }
+
+
+    traverse(entry, path?: string): void {
+        path = path || '';
+        if (entry.isFile) {
+            // Get file
+            entry.file((file) => {
+                this.files++;
+                //        console.log('File:', path + file.name);
+            });
+        } else if (entry.isDirectory) {
+            // Get folder contents
+            //      console.log('Directory:', path + entry.name);
+            this.folders++;
+
+            const dirReader = entry.createReader();
+            dirReader.readEntries((entries) => {
+                for (let i = 0; i < entries.length; i++) {
+                    this.traverse(entries[i], path + entry.name + '/');
+                }
+            });
+        }
+
+        if ((this.files + this.folders) % 500 === 0) {
+            console.log(`Files ${this.files} Folders ${this.folders}`);
+        }
+    }
+}
